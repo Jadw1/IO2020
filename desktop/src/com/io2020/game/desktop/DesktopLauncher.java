681dc3c88c421e2acefc8493e55a1e0e024f55e0
Jadw1 <jadw1_1@o2.pl>
2020-03-10 08:40:19 +0100

    generated project

---
+++ desktop/src/com/io2020/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.io2020.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.io2020.game.IOGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new IOGame(), config);
+	}
+}
