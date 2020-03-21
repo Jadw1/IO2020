c2961288f1895c76bb39977e70c387f91dc3ed6f
Quendora <quendora@gmail.com>
2020-03-21 18:36:11 +0100

    All frames in single atlas

--- core/src/com/io2020/entities/Player.java
+++ core/src/com/io2020/entities/Player.java
@@ -4,49 +4,35 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.TextureArray;
-import com.badlogic.gdx.graphics.g2d.Animation;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.g2d.TextureAtlas;
-import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.graphics.g2d.*;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.utils.Array;
 import org.graalvm.compiler.loop.InductionVariable;
 
-public class Player extends Actor
+public class Player extends Image
 {
     private float speed = 200.0f;
     private boolean flipped = false;
-    private SpriteBatch batch;
     private float stateTime = 0;
     private PlayerState state = PlayerState.STANDING;
 
-    private TextureAtlas idleAtlas;
+    public TextureRegion currentFrame;
     private Animation<TextureRegion> idleAnimation;
-    private TextureAtlas hitAtlas;
     private Animation<TextureRegion> hitAnimation;
-    private TextureAtlas runAtlas;
     private Animation<TextureRegion> runAnimation;
 
 
-    public Player(float start_x, float start_y, SpriteBatch batch) {
-        this.batch = batch;
-        Texture texture = new Texture("player.png");
-        setOrigin(texture.getWidth()/2, texture.getHeight()/2);
-        setSize(texture.getWidth(), texture.getHeight());
-        setScale(0.3f);
+
+    public Player(float start_x, float start_y, TextureAtlas atlas) {
         setPosition(start_x, start_y);
 
+        hitAnimation = new Animation<TextureRegion>(1f/8f, atlas.findRegions("knight_m_hit_anim"), Animation.PlayMode.LOOP);
+        idleAnimation = new Animation<TextureRegion>(1f/8f, atlas.findRegions("knight_m_idle_anim"), Animation.PlayMode.LOOP);
+        runAnimation = new Animation<TextureRegion>(1/8f, atlas.findRegions("knight_m_run_anim"), Animation.PlayMode.LOOP);
 
-        idleAtlas = new TextureAtlas(Gdx.files.internal("animation/knight_idle.atlas"));
-        idleAnimation = new Animation<TextureRegion>(1f/8f, idleAtlas.getRegions(), Animation.PlayMode.LOOP);
-
-        runAtlas = new TextureAtlas(Gdx.files.internal("animation/knight_run.atlas"));
-        runAnimation = new Animation<TextureRegion>(1/8f, runAtlas.getRegions(), Animation.PlayMode.LOOP);
-
-        hitAtlas = new TextureAtlas(Gdx.files.internal("animation/knight_hit.atlas"));
-        hitAnimation = new Animation<TextureRegion>(1/30f, hitAtlas.getRegions(), Animation.PlayMode.LOOP);
-
+        currentFrame = idleAnimation.getKeyFrame(stateTime);
     }
 
     @Override
@@ -59,18 +45,21 @@ public class Player extends Actor
         animate(delta);
     }
 
+    @Override
+    public void draw(Batch batch, float parentAlpha)
+    {
+        batch.draw(currentFrame, flipped ? getX() + 32 : getX(), getY(), flipped ? -32 : 32, 32);
+    }
+
     private void animate(float delta)
     {
-        TextureRegion currentFrame;
-
         stateTime += delta;
-        if (state == PlayerState.MOVING)
+
+        if (state == PlayerState.MOVING) {
             currentFrame = runAnimation.getKeyFrame(stateTime);
-        else
+        } else {
             currentFrame = idleAnimation.getKeyFrame(stateTime);
-
-
-        batch.draw(currentFrame, flipped ? getX() + 100 : getX(), getY(), flipped ? -100 : 100, 100);
+        }
     }
 
     private void handleInput(float delta) {
--- core/src/com/io2020/screens/GameScreen.java
+++ core/src/com/io2020/screens/GameScreen.java
@@ -1,5 +1,6 @@
 package com.io2020.screens;
 
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.io2020.entities.Player;
 import com.io2020.entities.World;
 import com.io2020.game.IOGame;
@@ -7,23 +8,26 @@ import com.io2020.game.IOGame;
 public class GameScreen extends BaseScreen {
     private Player player;
     private World world;
+    private TextureAtlas atlas;
 
     public GameScreen(IOGame game) {
         super(game);
 
+        atlas = new TextureAtlas("animation/Knight.pack");
+
         world = new World();
         stage.addActor(world);
 
-        player = new Player(0.0f,0.0f, spriteBatch);
+        player = new Player(0.0f,0.0f, atlas);
         stage.addActor(player);
     }
 
     @Override
     public void render(float delta) {
         super.render(delta);
-        spriteBatch.begin();
+
         update();
-        spriteBatch.end();
+
         spriteBatch.begin();
         stage.draw();
         spriteBatch.end();
